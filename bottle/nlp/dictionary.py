18c292e5b625933433e61e2deed32143c6e87b95
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2020-08-18 20:35:30 -0700

    Add dictionary builder based off maximum vocabulary size.

--- bottle/nlp/dictionary.py
+++ bottle/nlp/dictionary.py
@@ -4,9 +4,9 @@ from pytils import check
 from bottle.nlp import processing
 
 
-class Builder:
-    def __init__(self, minimum_count, exclude_numbers):
-        self.minimum_count = check.check_gte(minimum_count, 1)
+class OccurrenceCountBuilder:
+    def __init__(self, threshold, exclude_numbers):
+        self.threshold = check.check_gte(threshold, 1)
         self.exclude_numbers = check.check_one_of(exclude_numbers, [True, False])
         self.word_counts = {}
 
@@ -24,5 +24,28 @@ class Builder:
         return self
 
     def build(self):
-        return set([word for word, count in self.word_counts.items() if count >= self.minimum_count])
+        return set([word for word, count in self.word_counts.items() if count >= self.threshold])
+
+
+class VocabularySizeBuilder:
+    def __init__(self, threshold, exclude_numbers):
+        self.threshold = check.check_gte(threshold, 1)
+        self.exclude_numbers = check.check_one_of(exclude_numbers, [True, False])
+        self.word_counts = {}
+
+    def add(self, word):
+        if self.exclude_numbers and processing.is_number(word):
+            return self
+
+        if processing.is_reserved_token(word):
+            return self
+
+        if word not in self.word_counts:
+            self.word_counts[word] = 0
+
+        self.word_counts[word] += 1
+        return self
+
+    def build(self):
+        return set([item[0] for item in sorted(self.word_counts.items(), key=lambda item: item[1], reverse=True)[:self.threshold]])
 
--- test/nlp/dictionary.py
+++ test/nlp/dictionary.py
@@ -7,16 +7,16 @@ from bottle.nlp import dictionary
 
 
 def tests():
-    return create_suite([BuilderTests])
+    return create_suite([OccurrenceCountBuilderTests, VocabularySizeBuilderTests])
 
 
-class BuilderTests(TestCase):
+class OccurrenceCountBuilderTests(TestCase):
     def test_builder(self):
         word1 = "word1"
         word2 = "word2"
         word3 = "4.5"
 
-        builder = dictionary.Builder(1, False)
+        builder = dictionary.OccurrenceCountBuilder(1, False)
         builder.add(word1) \
             .add(word2) \
             .add(word1) \
@@ -29,7 +29,7 @@ class BuilderTests(TestCase):
         word2 = "word2"
         word3 = "4.5"
 
-        builder = dictionary.Builder(2, False)
+        builder = dictionary.OccurrenceCountBuilder(2, False)
         builder.add(word1) \
             .add(word2) \
             .add(word1) \
@@ -38,7 +38,7 @@ class BuilderTests(TestCase):
         self.assertEqual(builder.build(), set([word1]))
 
     def test_builder_exclude_numbers(self):
-        builder = dictionary.Builder(1, True)
+        builder = dictionary.OccurrenceCountBuilder(1, True)
         builder.add("23") \
             .add("-1") \
             .add("0") \
@@ -54,7 +54,63 @@ class BuilderTests(TestCase):
         word2 = "word2"
         word3 = "4.5"
 
-        builder = dictionary.Builder(1, False)
+        builder = dictionary.OccurrenceCountBuilder(1, False)
+        builder.add(word1) \
+            .add(word2) \
+            .add(word1) \
+            .add(word3) \
+            .add("<unk>") \
+            .add("<unknown>") \
+            .add("<blah>")
+
+        self.assertEqual(builder.build(), set([word1, word2, word3]))
+
+
+class VocabularySizeBuilderTests(TestCase):
+    def test_builder(self):
+        word1 = "word1"
+        word2 = "word2"
+        word3 = "4.5"
+
+        builder = dictionary.VocabularySizeBuilder(3, False)
+        builder.add(word1) \
+            .add(word2) \
+            .add(word1) \
+            .add(word3)
+
+        self.assertEqual(builder.build(), set([word1, word2, word3]))
+
+    def test_builder_minimum(self):
+        word1 = "word1"
+        word2 = "word2"
+        word3 = "4.5"
+
+        builder = dictionary.VocabularySizeBuilder(1, False)
+        builder.add(word1) \
+            .add(word2) \
+            .add(word1) \
+            .add(word3)
+
+        self.assertEqual(builder.build(), set([word1]))
+
+    def test_builder_exclude_numbers(self):
+        builder = dictionary.VocabularySizeBuilder(10, True)
+        builder.add("23") \
+            .add("-1") \
+            .add("0") \
+            .add("0.123") \
+            .add("100,000") \
+            .add("$6") \
+            .add("75%")
+
+        self.assertEqual(builder.build(), set(["$6", "75%"]))
+
+    def test_builder_reserved_token(self):
+        word1 = "word1"
+        word2 = "word2"
+        word3 = "4.5"
+
+        builder = dictionary.VocabularySizeBuilder(10, False)
         builder.add(word1) \
             .add(word2) \
             .add(word1) \
